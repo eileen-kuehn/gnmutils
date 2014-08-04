2bd27804c6ec60880ddcbaae8a317513ac559940
eileen <eileen.kuehn@cannysoft.de>
2014-08-04 22:31:15 +0200

    added class for representing process

---
+++ process.py
@@ -0,0 +1,23 @@
+class Process(object):
+    def __init__ (self, name=None, pid=None, ppid=None, uid=None, start_tme=0, exit_tme=none, error_code=None, signal=None, gpid=None, job_id=None):
+        self.name = name
+        self.pid = pid
+        self.ppid = ppid
+        self.uid = uid
+        self.start_tme = start_tme
+        self.exit_tme = exit_tme
+        self.error_code = error_code
+        self.signal = signal
+        self.gpid = gpid
+        self.job_id = job_id
+
+    def getDuration(self):
+        return self.exit_tme - self.start_tme
+
+    def setExitCode(self, exitCode):
+        self.error_code = exitCode >> 8
+        self.signal = exitCode & 255
+
+    def isComplete(self):
+        return self.start_tme and self.exit_tme
+
