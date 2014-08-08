081ec126acc0e3394bef1b4a09dae9e86376d5d7
Eileen Kuehn <eileen.kuehn@kit.edu>
2014-08-08 11:08:48 +0200

    added last_tme

--- job.py
+++ job.py
@@ -1,13 +1,17 @@
 class Job(object):
-    def __init__ (self, job_id=None, gpid=None, start_tme=None, exit_tme=None, uid=None):
+    def __init__ (self, job_id=None, gpid=None, start_tme=None, exit_tme=None, last_tme=None, uid=None):
         self.job_id = job_id
         self.gpid = gpid
         self.start_tme = start_tme
         self.exit_tme = exit_tme
         self.uid = uid
+        self.last_tme = last_tme
 
     def isComplete(self):
         return self.start_tme and self.exit_tme
+
+    def isValid(self):
+        return self.exit_tme >= last_tme
 
     def getDuration(self):
         return self.exit_tme - self.start_tme
