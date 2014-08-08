6be94722150ce4d8a049c26a74f3d20bfc4f88e5
Eileen Kuehn <eileen.kuehn@kit.edu>
2014-08-08 14:19:43 +0200

    added toDAO method to job

--- job.py
+++ job.py
@@ -1,18 +1,30 @@
+from dbutils.dbobjects import DBJobObject
+
 class Job(object):
-    def __init__ (self, job_id=None, gpid=None, start_tme=None, exit_tme=None, last_tme=None, uid=None):
+    def __init__ (self, id=None, job_id=None, run=None, gpid=None, start_tme=None, exit_tme=None, last_tme=None, uid=None, workernode_id=None, configuration_id=None):
+        self.id = id
+        self.run = run
         self.job_id = job_id
         self.gpid = gpid
         self.start_tme = start_tme
         self.exit_tme = exit_tme
         self.uid = uid
         self.last_tme = last_tme
+        self.workernode_id = workernode_id
+        self.configuration_id = configuration_id
 
     def isComplete(self):
         return self.start_tme and self.exit_tme
 
     def isValid(self):
-        return self.exit_tme >= last_tme
+        return self.exit_tme >= self.last_tme
 
     def getDuration(self):
         return self.exit_tme - self.start_tme
+
+    def toDAO(self):
+        jobDict = vars(self)
+        jobDict['completed'] = self.isComplete()
+        jobDict['valid'] = self.isValid()
+        return DBJobObject(**jobDict)
 
