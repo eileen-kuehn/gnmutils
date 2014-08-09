a2c8381e337454d186c5f51beb64ab62eb212882
Eileen Kuehn <eileen.kuehn@kit.edu>
2014-08-09 18:13:11 +0200

    Handling for correcting connection infos

--- traffic.py
+++ traffic.py
@@ -1,4 +1,5 @@
 import re
+import logging
 
 from evenmoreutils import string as stringutils
 
@@ -6,7 +7,7 @@ class Traffic(object):
     def __init__ (self, conn=None, pid=None, ppid=None, uid=None, tme=None,
             in_rate=None, out_rate=None, in_cnt=None, out_cnt=None, gpid=None,
             source_ip=None, dest_ip=None, source_port=None, dest_port=None,
-            conn_cat=None):
+            conn_cat=None, workernode=None):
         self.tme = tme
         self.pid = pid
         self.ppid = ppid
@@ -22,19 +23,42 @@ class Traffic(object):
         self.in_cnt = in_cnt
         self.out_cnt = out_cnt
         if conn:
-          self.setConnection(conn)
+          self.setConnection(conn=conn, workernode=workernode)
 
-    def setConnection(self, conn):
+    def setConnection(self, conn=None, workernode=None):
         splittedConnection = conn.split("-")
         splittedSource = splittedConnection[0].split(":")
         splittedTarget = splittedConnection[1].split(":")
+        if workernode:
+            lastIPPart = ".".join(map(str, map(int, workernode.split("-")[1:])))
+            internalIP = "10.1." + lastIPPart
+            if internalIP in splittedSource[0]:
+                # everything is fine
+                pass
+            elif internalIP in splittedTarget[0]:
+                # splitted values need to be exchanged
+                logging.warn("exchanging traffic values for %s and %s" %(splittedSource, splittedTarget))
+                tmp = splittedSource
+                splittedSource = splittedTarget
+                splittedTarget = tmp
+
+                tmpRate = self.in_rate
+                self.in_rate = self.out_rate
+                self.out_rate = tmpRate
+
+                tmpCnt = self.in_cnt
+                self.in_cnt = self.out_cnt
+                self.out_cnt = tmpCnt
+            else:
+                logging.warn("Calculated internal IP (%s) does not match anything (%s and %s)" %(internalIP, splittedSource, splittedTarget))
+
         self.source_ip = splittedSource[0]
         self.source_port = splittedSource[1]
         self.dest_ip = splittedTarget[0]
         self.dest_port = splittedTarget[1]
 
-        if (re.match("^10\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})",splittedTarget[0]))
-            or (re.match("^192\.108\.([0-9]{1,3})\.([0-9]{1,3})",splittedTarget[0])):
+        if ((re.match("^10\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})",splittedTarget[0]))
+                or (re.match("^192\.108\.([0-9]{1,3})\.([0-9]{1,3})",splittedTarget[0]))):
             # internal interface
             self.conn_cat = "int"
         else:
