59d8ef494c50999a43803a3e7f913c6493ad08a1
Eileen Kühn <eileen@scc-wkitx-cl-cn-199-153.scc.kit.edu>
2015-05-07 15:18:50 +0200

    renamed process to job

--- job.py
+++ job.py
@@ -1,32 +1,58 @@
-from dbobjects import DBJobObject
+from evenmoreutils import string as stringutils
 
 class Job(object):
-    def __init__ (self, id=None, job_id=None, run=None, gpid=None,
-            tme=None, exit_tme=None, last_tme=None, uid=None,
-            workernode_id=None, configuration_id=None):
-        self.id = id
-        self.run = run
-        self.job_id = job_id
-        self.gpid = gpid
+    def __init__ (self, name=None, cmd=None, pid=None, ppid=None, uid=None,
+            tme=None, exit_tme=None, error_code=None, signal=None,
+            gpid=None, job_id=None, valid=True, int_in_volume=None, int_out_volume=None,
+            ext_in_volume=None, ext_out_volume=None, tree_depth=None, process_type=None, color=None):
+        self.name = name
+        self.cmd = cmd
+        self.pid = int(pid)
+        self.ppid = int(ppid)
+        self.uid = int(uid)
         self.tme = tme
         self.exit_tme = exit_tme
-        self.uid = uid
-        self.last_tme = last_tme
-        self.workernode_id = workernode_id
-        self.configuration_id = configuration_id
+        self.error_code = error_code
+        self.signal = signal
+        self.gpid = int(gpid)
+        self.job_id = job_id
+        self.valid = valid
+        self.int_in_volume = stringutils.xfloat(int_in_volume)
+        self.int_out_volume = stringutils.xfloat(int_out_volume)
+        self.ext_in_volume = stringutils.xfloat(ext_in_volume)
+        self.ext_out_volume = stringutils.xfloat(ext_out_volume)
+        self.tree_depth = tree_depth
+        self.process_type = process_type
+        self.color = color
 
-    def isComplete(self):
-        return self.tme and self.exit_tme
-
-    def isValid(self):
-        return self.exit_tme >= self.last_tme
+    def setValid(self, valid):
+        self.valid = valid
 
     def getDuration(self):
-        return self.exit_tme - self.tme
+        return self.valid and (int(self.exit_tme) - int(self.tme))
 
-    def toDAO(self):
-        jobDict = vars(self)
-        jobDict['completed'] = self.isComplete()
-        jobDict['valid'] = self.isValid()
-        return DBJobObject(**jobDict)
+    def setExitCode(self, exitCode):
+        exitCode = int(exitCode)
+        self.error_code = exitCode >> 8
+        self.signal = exitCode & 255
+
+    def isComplete(self):
+        return self.valid and (self.tme and self.exit_tme) > 0
+
+    def getRow(self):
+        return ("%s,%s,%d,%d,%d,%d,%s,%s,%s,%s,%d,%s,%s,%s,%s,%s,%s,%s"
+                %(stringutils.xstr(self.tme),
+                    stringutils.xstr(self.exit_tme), self.pid,
+                    self.ppid, self.gpid, self.uid, self.name,
+                    self.cmd, stringutils.xstr(self.error_code),
+                    stringutils.xstr(self.signal), int(self.isComplete()),
+                    self.int_in_volume, self.int_out_volume,
+                    self.ext_in_volume, self.ext_out_volume,
+                    stringutils.xint(self.tree_depth),stringutils.xstr(self.process_type),
+                    stringutils.xstr(self.color)))
+
+    def getHeader(self):
+        return ("tme,exit_tme,pid,ppid,gpid,uid,name,cmd,error_code,"\
+                "signal,valid,int_in_volume,int_out_volume,ext_in_volume,"\
+                "ext_out_volume,tree_depth,process_type,color")
 
--- process.py
+++ process.py
@@ -2,28 +2,17 @@ from evenmoreutils import string as stringutils
 
 class Process(object):
     def __init__ (self, name=None, cmd=None, pid=None, ppid=None, uid=None,
-            tme=None, exit_tme=None, error_code=None, signal=None,
-            gpid=None, job_id=None, valid=True, int_in_volume=None, int_out_volume=None,
-            ext_in_volume=None, ext_out_volume=None, tree_depth=None, process_type=None, color=None):
+            tme=None, error_code=None, gpid=None, state=None, exit_tme=None):
         self.name = name
         self.cmd = cmd
         self.pid = int(pid)
         self.ppid = int(ppid)
         self.uid = int(uid)
         self.tme = tme
-        self.exit_tme = exit_tme
         self.error_code = error_code
-        self.signal = signal
         self.gpid = int(gpid)
-        self.job_id = job_id
-        self.valid = valid
-        self.int_in_volume = stringutils.xfloat(int_in_volume)
-        self.int_out_volume = stringutils.xfloat(int_out_volume)
-        self.ext_in_volume = stringutils.xfloat(ext_in_volume)
-        self.ext_out_volume = stringutils.xfloat(ext_out_volume)
-        self.tree_depth = tree_depth
-        self.process_type = process_type
-        self.color = color
+        self.state = state
+        self.exit_tme = exit_tme
 
     def setValid(self, valid):
         self.valid = valid
@@ -52,7 +41,6 @@ class Process(object):
                     stringutils.xstr(self.color)))
 
     def getHeader(self):
-        return ("tme,exit_tme,pid,ppid,gpid,uid,name,cmd,error_code,"\
-                "signal,valid,int_in_volume,int_out_volume,ext_in_volume,"\
-                "ext_out_volume,tree_depth,process_type,color")
+        return ("tme,pid,ppid,uid,name,cmd,exit_code,state,gpid")
+        return {"tme": 0, "pid": 1, "ppid": 2, "uid": 3, "name": 4, "cmd": 5, "exit_code": 6, "state": 7, "gpid": 8}
 
