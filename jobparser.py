44496c49044aeec1ef3355a45c938f7a970abcf9
Eileen Kühn <eileen@scc-wkitx-cl-cn-199-153.scc.kit.edu>
2015-05-07 15:24:55 +0200

    added jobparser (former processparser) and fixed some values for process object

---
+++ jobparser.py
@@ -0,0 +1,84 @@
+import bisect
+import logging
+
+from evenmoreutils.tree import Node
+from evenmoreutils.tree import Tree
+
+from process import *
+
+class JobParser(object):
+    def __init__(self):
+        self._root = None
+        self._processCache = {}
+        self._faultyNodes = set()
+        self._treeInitialized = False
+        
+    def defaultHeader(self):
+        return {"tme":0, "exit_tme": 1 ,"pid": 2, "ppid": 3, "gpid": 4, "uid": 5, "name": 6, "cmd": 7, "error_code": 8, "signal": 9, "valid": 10, "int_in_volume": 11, "int_out_volume": 12, "ext_in_volume": 13, "ext_out_volume":14}
+
+    def clearCaches(self):
+        logging.info("clearing caches of process parser")
+        del self._root
+        self._root = None
+        del self._faultyNodes
+        self._faultyNodes = set()
+        del self._processCache
+        self._processCache = {}
+        self._treeInitialized = False
+
+    def parseRow(self, row=None, headerCache=None, tme=None):
+        dataDict = {}
+        for key in headerCache:
+            dataDict[key] = row[headerCache[key]]
+        
+        process = Process(**dataDict)
+        self._addProcess(process, "sge_shepherd" in row[headerCache['name']])
+
+    # access to tree
+    @property
+    def tree(self):
+        if not self._treeInitialized:
+            self._initializeTree()
+            self._treeInitialized = True
+        if len(self._faultyNodes) <= 1:
+            return Tree(self._root)
+        logging.info("faulty nodes: %s" %self._faultyNodes)
+        return None
+
+    def _addProcess(self, processObject, isRoot):
+        newNode = Node(value=processObject)
+        if isRoot: self._root = newNode
+        
+        try:
+            tmeList = [int(process.value.tme) for process in self._processCache[processObject.pid]]
+            index = bisect.bisect_left(tmeList, int(processObject.tme))
+            self._processCache[processObject.pid].insert(index, newNode)
+        except KeyError:
+            self._processCache[processObject.pid] = [newNode]
+
+    def _getNodeForTME(self, tme, pid):
+        tme = int(tme)
+        try:
+            processArray = self._processCache[pid]
+            tmeArray = [int(node.value.tme) for node in processArray]
+            index = bisect.bisect_right(tmeArray, tme) - 1
+            return processArray[index]
+        except KeyError:
+            self._faultyNodes.add(pid)
+            logging.error("error for %s (%d)" %(pid, tme))
+
+    def _initializeTree(self):
+        logging.info("Initializing tree structure")
+        # sort the keys first to get the correct ordering in the final tree
+        for pid in sorted(self._processCache.keys(), key=lambda item: int(item)):
+            for node in self._processCache[pid]:
+                parent = self._getNodeForTME(tme=int(node.value.tme), pid=node.value.ppid)
+                if parent:
+                    parent.add(node)
+                else:
+                    logging.error("no parent was found for node %s (%s)" %(node.value.pid, node.value.tme))
+                    
+        if len(self._faultyNodes) <= 1:
+            # set depth
+            for node, depth in Tree(self._root).walkDFS():
+                node.value.tree_depth = depth
--- process.py
+++ process.py
@@ -2,14 +2,14 @@ from evenmoreutils import string as stringutils
 
 class Process(object):
     def __init__ (self, name=None, cmd=None, pid=None, ppid=None, uid=None,
-            tme=None, error_code=None, gpid=None, state=None, exit_tme=None):
+            tme=None, exit_code=None, gpid=None, state=None, exit_tme=None):
         self.name = name
         self.cmd = cmd
         self.pid = int(pid)
         self.ppid = int(ppid)
         self.uid = int(uid)
         self.tme = tme
-        self.error_code = error_code
+        self.exit_code = exit_code
         self.gpid = int(gpid)
         self.state = state
         self.exit_tme = exit_tme
@@ -29,18 +29,11 @@ class Process(object):
         return self.valid and (self.tme and self.exit_tme) > 0
 
     def getRow(self):
-        return ("%s,%s,%d,%d,%d,%d,%s,%s,%s,%s,%d,%s,%s,%s,%s,%s,%s,%s"
+        return ("%s,%d,%d,%d,%s,%s,%s,%s,%d"
                 %(stringutils.xstr(self.tme),
-                    stringutils.xstr(self.exit_tme), self.pid,
-                    self.ppid, self.gpid, self.uid, self.name,
-                    self.cmd, stringutils.xstr(self.error_code),
-                    stringutils.xstr(self.signal), int(self.isComplete()),
-                    self.int_in_volume, self.int_out_volume,
-                    self.ext_in_volume, self.ext_out_volume,
-                    stringutils.xint(self.tree_depth),stringutils.xstr(self.process_type),
-                    stringutils.xstr(self.color)))
+                self.pid, self.ppid, self.uid, self.name, self.cmd,
+                self.exit_code, self.state, self.gpid))
 
     def getHeader(self):
         return ("tme,pid,ppid,uid,name,cmd,exit_code,state,gpid")
-        return {"tme": 0, "pid": 1, "ppid": 2, "uid": 3, "name": 4, "cmd": 5, "exit_code": 6, "state": 7, "gpid": 8}
 
