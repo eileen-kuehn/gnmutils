d2b747855c84488f9bff246bcf56be82ffb23b69
Eileen Kühn <eileen@scc-wkitx-cl-cn-199-153.scc.kit.edu>
2015-05-13 09:47:19 +0200

    cleaned up ressources, renaming

--- process.py
+++ process.py
@@ -2,23 +2,123 @@ from evenmoreutils import string as stringutils
 
 class Process(object):
     def __init__ (self, name=None, cmd=None, pid=None, ppid=None, uid=None,
-            tme=None, exit_code=None, gpid=None, state=None):
+            tme=None, exit_tme=None, error_code=None, signal=None, exit_code=0,
+            gpid=None, state=None, job_id=None, int_in_volume=None, int_out_volume=None,
+            ext_in_volume=None, ext_out_volume=None, tree_depth=None,
+            process_type=None, color=None, valid=False):
         self.name = name
         self.cmd = cmd
-        self.pid = int(pid)
-        self.ppid = int(ppid)
-        self.uid = int(uid)
-        self.tme = tme
-        self.exit_code = stringutils.xint(exit_code)
-        self.gpid = int(gpid)
-        self.state = stringutils.xstr(state)
+        self._pid = pid
+        self._ppid = ppid
+        self._uid = uid
+        self._gpid = gpid
+        self._tme = tme
+        self._exit_tme = exit_tme
+        self.valid = valid
+        self._state = stringutils.xstr(state)
+        
+        # set exit_code first, but can be overwritten by error_code and signal
+        self._setExitCode(exit_code)
+        self._error_code = error_code
+        self._signal = signal
+        
+        self.job_id = job_id
+        self.tree_depth = tree_depth
+        self.process_type = process_type
+        self.color = color
+        
+        self.int_in_volume = stringutils.xfloat(int_in_volume)
+        self.int_out_volume = stringutils.xfloat(int_out_volume)
+        self.ext_in_volume = stringutils.xfloat(ext_in_volume)
+        self.ext_out_volume = stringutils.xfloat(ext_out_volume)
+        
+    @property
+    def pid(self):
+        return int(self._pid)
+        
+    @property
+    def ppid(self):
+        return int(self._ppid)
+        
+    @property
+    def uid(self):
+        return int(self._uid)
+        
+    @property
+    def gpid(self):
+        return int(self._gpid)
+        
+    @property
+    def tme(self):
+        return int(self._tme)
+        
+    @property
+    def exit_tme(self):
+        return int(self._exit_tme)
+        
+    @property
+    def exit_code(self):
+        return int(self._exit_code)
+        
+    @property
+    def signal(self):
+        return int(self._signal)
 
-    def getRow(self):
-        return ("%s,%d,%d,%d,%s,%s,%s,%s,%d"
-                %(stringutils.xstr(self.tme), self.pid, self.ppid, self.uid,
-                self.name, self.cmd, stringutils.xint(self.exit_code),
-                stringutils.xstr(self.state), self.gpid))
+    @property
+    def exit_code(self):
+        return self._error_code and self._signal and ((self._error_code << 8) + self._signal)
+    
+    def getDuration(self):
+        return self.valid and (self.exit_tme - self.tme)
+    
+    def getProcessEventRow(self):
+        return ("%s,%s,%s,%s,%s,%s,%d,%s,%s"
+                %(stringutils.xint(self._tme), stringutils.xint(self._pid),
+                stringutils.xint(self._ppid), stringutils.xint(self._uid),
+                self.name, self.cmd,
+                self.exit_code(), self.state, stringutils.xint(self._gpid)))
 
-    def getHeader(self):
+    def getProcessEventHeader(self):
         return ("tme,pid,ppid,uid,name,cmd,exit_code,state,gpid")
+        
+    def getRow(self):
+        return ("%s,%s,%d,%d,%d,%d,%s,%s,%s,%s,%s,%d,%s,%s,%s,%s,%s,%s"
+                %(stringutils.xint(self._tme),
+                    stringutils.xint(self._exit_tme), stringutils.xint(self._pid),
+                    stringutils.xint(self._ppid), stringutils.xint(self._gpid),
+                    stringutils.xint(self._uid), self.name, self.cmd,
+                    stringutils.xint(self._error_code), 
+                    stringutils.xint(self._signal), self.int_in_volume,
+                    self.valid, self.int_out_volume, self.ext_in_volume,
+                    self.ext_out_volume, stringutils.xint(self.tree_depth),
+                    stringutils.xstr(self.process_type), stringutils.xstr(self.color)))
+    
+    def getHeader(self):
+        return ("tme,exit_tme,pid,ppid,gpid,uid,name,cmd,error_code,"\
+                "signal,int_in_volume,int_out_volume,ext_in_volume,"\
+                "ext_out_volume,tree_depth,process_type,color")
+        
+    def addProcessEvent(self, name=None, cmd=None, pid=None, ppid=None, 
+            uid=None, tme=None, exit_code=None, gpid=None, state=None):
+        if "exit" == state:
+            if self.state != ".":
+                self.valid = True
+            self._setExitCode(exit_code)
+            self._exit_tme = tme
+        else:
+            # maybe exit process event arrives first...
+            if "exit" == self.state and state != ".":
+                self.valid = True
+            self._tme = self._tme or tme
+        self.state = self.state or state
+        self.name = self.name or name
+        self.cmd = self.cmd or cmd
+        self._pid = self._pid or pid
+        self._ppid = self._ppid or ppid
+        self._uid = self._uid or uid
+        self._gpid = self._gpid or gpid
+
+    def _setExitCode(self, exit_code):
+        self._error_code = int(exit_code) >> 8
+        self._signal = int(exit_code) & 255
 
