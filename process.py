d1718582ca46be12c7211daeee1f4f651fcd3c5c
Eileen Kühn <eileen@vpn-cl-192-20.scc.kit.edu>
2015-05-14 13:56:04 +0200

    different conversions

--- process.py
+++ process.py
@@ -1,50 +1,55 @@
 from evenmoreutils import string as stringutils
 
-class ProcessEvent(object):
-    def __init__ (self, process=None):
-        self._process = process
-        
-    def getRow(self):
-        return ("%s,%s,%s,%s,%s,%s,%s,%s,%s"
-                %(stringutils.xint(self._process.tme), stringutils.xint(self._process.pid),
-                stringutils.xint(self._process.ppid), stringutils.xint(self._process.uid),
-                self._process.name, self._process.cmd, stringutils.xint(self._process.exit_code), 
-                self._process.state, stringutils.xint(self._process.gpid)))
-    
-    def getHeader(self):
-        return ("tme,pid,ppid,uid,name,cmd,exit_code,state,gpid")
-
 class Process(object):
     def __init__ (self, name=None, cmd=None, pid=None, ppid=None, uid=None,
             tme=None, exit_tme=None, error_code=None, signal=None, exit_code=0,
             gpid=None, state=None, job_id=None, int_in_volume=None, int_out_volume=None,
             ext_in_volume=None, ext_out_volume=None, tree_depth=None,
             process_type=None, color=None, valid=False):
-        self._name = name
-        self.cmd = cmd
-        self._pid = pid
-        self._ppid = ppid
-        self._uid = uid
-        self._gpid = gpid
-        self._tme = tme
-        self._exit_tme = exit_tme
+        self._name = self._checkIsNone(value=name)
+        self.cmd = self._checkIsNone(value=cmd)
+        self._pid = self._checkIsNone(value=pid)
+        self._ppid = self._checkIsNone(value=ppid)
+        self._uid = self._checkIsNone(value=uid)
+        self._gpid = self._checkIsNone(value=gpid)
         self.valid = valid
+        
+        if state is not None and len(state) > 0:
+            if "exit" in state:
+                # set exit_tme
+                self._exit_tme = self._checkIsNone(value=tme)
+            elif "fork" in state:
+                # set tme
+                self._tme = self._checkIsNone(value=tme)
+            elif ".":
+                self._tme = self._checkIsNone(value=tme)
+                self.valid = False
+        else:
+            self._tme = self._checkIsNone(value=tme)
+            self._exit_tme = self._checkIsNone(value=exit_tme)
         self._state = stringutils.xstr(state)
         
         # set exit_code first, but can be overwritten by error_code and signal
-        self._setExitCode(exit_code)
-        self._error_code = error_code
-        self._signal = signal
+        if self._checkIsNone(value=exit_code) is not None:
+            self._setExitCode(exit_code)
+        else:
+            self._error_code = self._checkIsNone(value=error_code)
+            self._signal = self._checkIsNone(value=signal)
         
-        self.job_id = job_id
-        self.tree_depth = tree_depth
-        self.process_type = process_type
-        self.color = color
+        self.job_id = self._checkIsNone(value=job_id)
+        self.tree_depth = self._checkIsNone(value=tree_depth)
+        self.process_type = self._checkIsNone(value=process_type)
+        self.color = self._checkIsNone(value=color)
         
         self.int_in_volume = stringutils.xfloat(int_in_volume)
         self.int_out_volume = stringutils.xfloat(int_out_volume)
         self.ext_in_volume = stringutils.xfloat(ext_in_volume)
         self.ext_out_volume = stringutils.xfloat(ext_out_volume)
+        
+    def _checkIsNone(self, value=None):
+        if value is not None and len(value) > 0:
+            return value
+        return None
     
     @property
     def state(self):
@@ -79,10 +84,6 @@ class Process(object):
         return int(self._exit_tme)
         
     @property
-    def exit_code(self):
-        return int(self._exit_code)
-        
-    @property
     def signal(self):
         return int(self._signal)
 
@@ -94,24 +95,25 @@ class Process(object):
         return self.valid and (self.exit_tme - self.tme)
         
     def getRow(self):
-        return ("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%d,%s,%s,%s,%s,%s,%s"
+        return ("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%d,%s,%s,%s,%s,%s,%s,%s,%s"
                 %(stringutils.xint(self._tme),
                     stringutils.xint(self._exit_tme), stringutils.xint(self._pid),
                     stringutils.xint(self._ppid), stringutils.xint(self._gpid),
                     stringutils.xint(self._uid), self.name, self.cmd,
                     stringutils.xint(self._error_code), 
-                    stringutils.xint(self._signal), self.int_in_volume,
-                    self.valid, self.int_out_volume, self.ext_in_volume,
+                    stringutils.xint(self._signal), self.valid, self.int_in_volume,
+                    self.int_out_volume, self.ext_in_volume,
                     self.ext_out_volume, stringutils.xint(self.tree_depth),
-                    stringutils.xstr(self.process_type), stringutils.xstr(self.color)))
+                    stringutils.xstr(self.process_type), stringutils.xstr(self.color),
+                    self._state))
                     
     def toProcessEvent(self):
         return ProcessEvent(process=self)
     
     def getHeader(self):
         return ("tme,exit_tme,pid,ppid,gpid,uid,name,cmd,error_code,"\
-                "signal,int_in_volume,int_out_volume,ext_in_volume,"\
-                "ext_out_volume,tree_depth,process_type,color")
+                "signal,valid,int_in_volume,int_out_volume,ext_in_volume,"\
+                "ext_out_volume,tree_depth,process_type,color,state")
         
     def addProcessEvent(self, name=None, cmd=None, pid=None, ppid=None, 
             uid=None, tme=None, exit_code=None, gpid=None, state=None):
