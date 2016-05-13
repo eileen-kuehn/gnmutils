e5073adec56ef66adf14f5fdd5445cb577bf4837
Eileen Kuehn <eileen@scc-wkitx-cl-cn-197-26.scc.kit.edu>
2016-05-13 14:56:53 +0200

    restructuring

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import setuptools
+from distutils.core import setup
+
+setup(
+        name="gnmutils",
+        version="0.1",
+        description="Utilities for GridKa Network Monitor",
+        author="Eileen Kuehn",
+        author_email="eileen.kuehn@kit.edu",
+        url="https://bitbucket.org/eileenkuehn/gnmutils",
+        packages=setuptools.find_packages(),
+)
+
