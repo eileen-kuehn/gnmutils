0910eb23da077e9f70a180a0dde7d3a996248a13
Eileen Kuehn <eileen@Eileens-MacBook-Air-3.local>
2016-06-24 01:13:12 +0200

    fixed version

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ setup(
         author_email="eileen.kuehn@kit.edu",
         url="https://bitbucket.org/eileenkuehn/gnmutils",
         packages=setuptools.find_packages(),
-        dependency_links = ['git+ssh://git@bitbucket.org/eileenkuehn/evenmoreutils.git@master#egg=evenmoreutils-bb-0.1', 
-                            'git+ssh://git@bitbucket.org/eileenkuehn/dbutils.git@master#egg=dbutils-bb-0.1',
-                            'git+ssh://git@bitbucket.org/eileenkuehn/utility.git@master#egg=utility-bb-0.1'],
-        install_requires=['psycopg2', 'evenmoreutils', 'dbutils', 'utility'],
+        dependency_links = ['git+ssh://git@bitbucket.org/eileenkuehn/evenmoreutils.git@master#egg=evenmoreutils-0.1', 
+                            'git+ssh://git@bitbucket.org/eileenkuehn/dbutils.git@master#egg=dbutils-0.1',
+                            'git+ssh://git@bitbucket.org/eileenkuehn/utility.git@master#egg=utility-0.1'],
+        install_requires=['psycopg2', 'evenmoreutils==0.1', 'dbutils==0.1', 'utility==0.1'],
 )
 
