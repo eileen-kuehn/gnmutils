3f19df5d200c079cda21eaa4467ff79a82c55631
Eileen Kuehn <eileen@Eileens-MacBook-Air-3.local>
2016-06-24 00:34:06 +0200

    changed setup to include dependency links

--- setup.py
+++ setup.py
@@ -11,6 +11,9 @@ setup(
         author_email="eileen.kuehn@kit.edu",
         url="https://bitbucket.org/eileenkuehn/gnmutils",
         packages=setuptools.find_packages(),
-        install_requires=['psycopg2'],
+        dependency_links = ['git+ssh://git@bitbucket.org/eileenkuehn/evenmoreutils.git@master#egg=evenmoreutils-0.1', 
+                            'git+ssh://git@bitbucket.org/eileenkuehn/dbutils.git@master#egg=dbutils-0.1',
+                            'git+ssh://git@bitbucket.org/eileenkuehn/utility.git@master#egg=utility-0.1'],
+        install_requires=['psycopg2', 'evenmoreutils', 'dbutils', 'utility'],
 )
 
