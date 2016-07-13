5eb7a11abfe27462f1f9f632eb968aabbe78892e
Eileen Kuehn <eileen@scc-wkitx-cl-cn-195-226.scc.kit.edu>
2016-07-13 15:59:43 +0200

    added test for python interpreter for requirements

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 
 import setuptools
+import platform
 from distutils.core import setup
+
+INSTALL_REQUIRES = ['evenmoreutils==0.1', 'utility==0.1']
+if 'pypy' not in platform.python_implementation().lower():
+    INSTALL_REQUIRES.append('psycopg2')
+    INSTALL_REQUIRES.append('dbutils==0.1')'
 
 setup(
         name="gnmutils",
@@ -11,9 +17,9 @@ setup(
         author_email="eileen.kuehn@kit.edu",
         url="https://bitbucket.org/eileenkuehn/gnmutils",
         packages=setuptools.find_packages(),
-        dependency_links = ['git+ssh://git@bitbucket.org/eileenkuehn/evenmoreutils.git@master#egg=evenmoreutils-0.1', 
+        dependency_links = ['git+ssh://git@bitbucket.org/eileenkuehn/evenmoreutils.git@master#egg=evenmoreutils-0.1',
                             'git+ssh://git@bitbucket.org/eileenkuehn/dbutils.git@master#egg=dbutils-0.1',
                             'git+ssh://git@bitbucket.org/eileenkuehn/utility.git@master#egg=utility-0.1'],
-        install_requires=['psycopg2', 'evenmoreutils==0.1', 'dbutils==0.1', 'utility==0.1'],
+        install_requires=INSTALL_REQUIRES,
 )
 
